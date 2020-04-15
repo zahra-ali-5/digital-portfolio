5da694c4e7040230232922d6d321569470230fc9
zahra-ali-5 <60189879+zahra-ali-5@users.noreply.github.com>
2020-04-15 14:13:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let firstName; //->user info
+let lastName; //definition
+let age=44; //declaration and definition
+let lightOn=true;// single = is assignment, double==check, we get ===as well
+
+function lightSwitch() {
+
+    if(lightOn == true){
+       lightOn=false
+       console.log(lightOn)
+    }
+    else   {
+       lightOn=true
+       console.log(lightOn)
+    }
+}
+
+lightSwitch()
+lightSwitch()
+lightSwitch()
+
+let numberOne;
+let numberTwo;
+let total;
+
+function addition(numberOne,numberTwo){
+    total = numberOne + numberTwo
+    console.log(total)
+}
+addition(20, 30) // 50
+addition(123, 56) // 179
+addition(44, 45) // 89
+
+//algorithm->
+
+
+
+//declaration & definintion
+
+// varibale: storage box
+//firstName: zahra
+//lastName: Ali
+//
